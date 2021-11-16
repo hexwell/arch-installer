0c59188be30c91457f7a3e4c9d6862df1a4fd9a3
Ettore Forigo <ettore.forigo@gmail.com>
2021-11-16 15:29:50 +0100

    fix bootloader setup file 3

--- modules/install/efistub_bootloader.bash
+++ modules/install/efistub_bootloader.bash
@@ -36,7 +36,7 @@ set -u
 
 local bootloader_setup_script="$INSTALLATION_MOUNTPOINT""/boot/setup_bootloader.bash"
 
-echo \#!/bin/bash > "$bootloader_setup_script"
+echo "#!/bin/bash" > "$bootloader_setup_script"
 echo efibootmgr --disk "$boot_partition_disk_device" --part "$boot_partition_number" --create --label \"arch linux\" --loader /vmlinuz-linux --unicode \""$encryption_kernel_param""root=""$root_partition_device"" rw ""$ucode_kernel_param""initrd=\initramfs-linux.img""$resume_kernel_params"\" --verbose >> "$bootloader_setup_script"
 echo >> "$bootloader_setup_script"
 

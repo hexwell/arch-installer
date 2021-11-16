d7afa8eccee77226bd570075fec08dc879233365
Ettore Forigo <ettore.forigo@gmail.com>
2021-11-16 15:25:50 +0100

    fix bootloader setup file

--- modules/install/efistub_bootloader.bash
+++ modules/install/efistub_bootloader.bash
@@ -34,9 +34,10 @@ set -u
 
 # Save bootloader configuration so the installation can be recovered if the firmware deletes the entry
 
-local bootloader_setup_script="$INSTALLATION_MOUNTPOINT""/boot/setup_bootloader.sh"
+local bootloader_setup_script="$INSTALLATION_MOUNTPOINT""/boot/setup_bootloader.bash"
 
-echo efibootmgr --disk "$boot_partition_disk_device" --part "$boot_partition_number" --create --label "arch linux" --loader /vmlinuz-linux --unicode "$encryption_kernel_param""root=""$root_partition_device"" rw ""$ucode_kernel_param""initrd=\initramfs-linux.img""$resume_kernel_params" --verbose > "$bootloader_setup_script"
+echo #!/bin/bash > "$bootloader_setup_script"
+echo efibootmgr --disk "$boot_partition_disk_device" --part "$boot_partition_number" --create --label \"arch linux\" --loader /vmlinuz-linux --unicode \""$encryption_kernel_param""root=""$root_partition_device"" rw ""$ucode_kernel_param""initrd=\initramfs-linux.img""$resume_kernel_params"\" --verbose >> "$bootloader_setup_script"
 echo >> "$bootloader_setup_script"
 
 chmod +x "$bootloader_setup_script"

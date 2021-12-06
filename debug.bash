e615f2f1b2ddaf8af10f80a8212c3a7bec36d85b
Ettore Forigo <ettore.forigo@gmail.com>
2021-12-06 11:52:56 +0100

    fix log naming issue

--- debug.bash
+++ debug.bash
@@ -4,7 +4,9 @@ unset TMUX
 
 tmux new-session -d -s arch "bash <(echo 'sleep 1; ""$1""; read -p \"Press enter to exit...\"')"
 
-tmux split-window -h -t arch:0 "echo > ""$1"".log && tail -f ""$1"".log"
+logfile=${1%.bash}.log
+
+tmux split-window -h -t arch:0 "echo > ""$logfile"" && tail -f ""$logfile"
 tmux select-pane -t arch:0.0
 
 tmux set -g -t arch mouse on

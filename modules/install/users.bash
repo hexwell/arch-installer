cec8d613a442805785035d345b7553a7b8730874
Ettore Forigo <ettore.forigo@gmail.com>
2022-06-17 13:14:45 +0200

    fix file format change

--- modules/install/users.bash
+++ modules/install/users.bash
@@ -5,7 +5,7 @@
 #   USERNAME
 #   USERPASS
 
-sed -i '/%wheel ALL=(ALL) ALL/s/^# //' $INSTALLATION_MOUNTPOINT/etc/sudoers
+sed -i '/%wheel ALL=(ALL:ALL) ALL/s/^# //' $INSTALLATION_MOUNTPOINT/etc/sudoers
 
 arch-chroot $INSTALLATION_MOUNTPOINT useradd -m -G wheel -s /bin/bash "$USERNAME"
 echo "$USERNAME"":""$USERPASS" | arch-chroot $INSTALLATION_MOUNTPOINT chpasswd

f00f85f8143bd0fcea9d1a66d0711c3a5f0fae40
Ettore Forigo <ettore.forigo@gmail.com>
2022-06-19 16:33:29 +0200

    Update wm.bash

--- modules/personalize/wm.bash
+++ modules/personalize/wm.bash
@@ -4,29 +4,10 @@ wm_module() {
 
 # IMPORTS:
 #   out
-#   USER
 
 out "[.] Installing WM."
 
-pacman -S --noconfirm base-devel openssh git xorg-server xorg-xinit xorg-xrandr xorg-xsetroot libxinerama i3-wm i3status noto-fonts dmenu
-
-git clone https://git.suckless.org/st /home/$USER/.config/st
-
-current_dir=$(pwd)
-
-cd /home/$USER/.config/st
-make install
-
-cd $current_dir
-
-chown -R $USER /home/$USER/.config
-
-sed -i '/# start some nice programs/,$s/^/# /' /etc/X11/xinit/xinitrc
-
-echo exec i3 > /home/$USER/.xinitrc
-echo >> /home/$USER/.xinitrc
-
-chown $USER /home/$USER/.xinitrc
+pacman -S --noconfirm xorg-server xorg-xinit xorg-xrandr xorg-xsetroot libxinerama i3-wm i3status noto-fonts dmenu alacritty
 
 out "[+] WM installed."
 

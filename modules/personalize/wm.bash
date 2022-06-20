06bdefb63c45b92350461a2f52c066d0a918cb33
Ettore Forigo <ettore.forigo@gmail.com>
2022-06-20 16:44:17 +0200

    Update wm.bash

--- modules/personalize/wm.bash
+++ modules/personalize/wm.bash
@@ -5,6 +5,6 @@
 
 out "[.] Installing WM."
 
-pacman -S --noconfirm xorg-server xorg-xinit xorg-xrandr xorg-xsetroot libxinerama i3-wm i3status noto-fonts dmenu alacritty
+pacman -S --noconfirm xorg-server xorg-xinit xorg-xrandr xorg-xsetroot libxinerama i3-wm i3status noto-fonts dmenu alacritty mate-polkit
 
 out "[+] WM installed."

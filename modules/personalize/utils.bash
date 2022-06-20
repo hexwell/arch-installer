86b0ebb35ff8c5642aebdbc9991d9d92bf6057bd
Ettore Forigo <ettore.forigo@gmail.com>
2022-06-20 20:55:36 +0200

    Update utils.bash

--- modules/personalize/utils.bash
+++ modules/personalize/utils.bash
@@ -5,6 +5,11 @@
 
 out '[.] Installing utils.'
 
-pacman -S --noconfirm pulseaudio alsa-utils dmenu alacritty mate-polkit
+pacman -S --noconfirm pulseaudio alsa-utils dmenu alacritty mate-polkit wget
+
+wget https://tools.suckless.org/dmenu/scripts/dmenu_run_with_command_history/dmenu_run_history -P /usr/bin/
+chmod +x /usr/bin/dmenu_run_history
+
+pacman -Rs wget
 
 out '[+] Utils installed.'

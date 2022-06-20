ff1054a8873ad20ad3adcd39ae0a774588e73ac1
Ettore Forigo <ettore.forigo@gmail.com>
2022-06-20 23:31:17 +0200

    add copy of installer on USB

---
+++ modules/usb/add_partition.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# IMPORTS:
+#   out
+#   device
+
+# EXPORTS:
+#   partition_device : Device of the new partition
+
+out "[.] Adding partition"
+
+(
+echo n  # Create a new partition
+echo    # Default type (primary)
+echo    # Default number
+echo    # Default first sector
+echo    # Default last sector
+echo w
+) | fdisk "$device"
+
+partition_device="$device""3"
+
---
+++ modules/usb/format_and_mount.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# IMPORTS:
+#   out
+#   partition_device
+
+out '[.] Formatting.'
+
+mkfs.ext4 "$partition_device"
+mount "$partition_device" /mnt
+
+out '[+] Done.'
+

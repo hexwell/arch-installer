a3ce9164a871e46172b04f4e6c6c27a52de53e9c
Ettore Forigo <ettore.forigo@gmail.com>
2022-06-20 22:41:09 +0200

    Update users.bash

--- modules/install/users.bash
+++ modules/install/users.bash
@@ -9,3 +9,7 @@ sed -i '/%wheel ALL=(ALL:ALL) ALL/s/^# //' $INSTALLATION_MOUNTPOINT/etc/sudoers
 
 arch-chroot $INSTALLATION_MOUNTPOINT useradd -m -G wheel -s /bin/bash "$USERNAME"
 echo "$USERNAME"":""$USERPASS" | arch-chroot $INSTALLATION_MOUNTPOINT chpasswd
+
+echo \# fix for DEL key not working in terminals > /home/$USERNAME/.inputrc
+echo >> /home/$USERNAME/.inputrc
+echo set enable-keypad on >> /home/$USERNAME/.inputrc

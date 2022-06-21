a043c07ffe7d85947ac0b547f1e813aec5c6450c
Ettore Forigo <ettore.forigo@gmail.com>
2022-06-21 17:07:56 +0200

    Update select_device.bash

--- modules/usb/select_device.bash
+++ modules/usb/select_device.bash
@@ -8,6 +8,6 @@
 #   device : Installation target device
 
 if ! [[ -v DEVICE ]]; then
-    devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)
+    devicelist=$(lsblk --nodeps --paths --list --noheadings --sort size --output name,size | grep --extended-regexp --invert-match "boot|rpmb|loop" | tac)
     device=$(dialog --keep-tite --stdout --menu "Select installation disk" 0 0 0 ${devicelist}) || exit 1
 fi

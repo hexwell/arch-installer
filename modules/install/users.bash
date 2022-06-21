9a51e71947551dae1d007a31e858a5cd83b31ddb
Ettore Forigo <ettore.forigo@gmail.com>
2022-06-21 16:56:24 +0200

    Update users.bash

--- modules/install/users.bash
+++ modules/install/users.bash
@@ -5,9 +5,9 @@
 #   USERNAME
 #   USERPASS
 
-sed -i '/%wheel ALL=(ALL:ALL) ALL/s/^# //' $INSTALLATION_MOUNTPOINT/etc/sudoers
+sed --in-place '/%wheel ALL=(ALL:ALL) ALL/s/^# //' $INSTALLATION_MOUNTPOINT/etc/sudoers
 
-arch-chroot $INSTALLATION_MOUNTPOINT useradd -m -G wheel -s /bin/bash "$USERNAME"
+arch-chroot $INSTALLATION_MOUNTPOINT useradd --create-home --groups wheel --shell /bin/bash "$USERNAME"
 echo "$USERNAME"":""$USERPASS" | arch-chroot $INSTALLATION_MOUNTPOINT chpasswd
 
 echo \# fix for DEL key not working in terminals > $INSTALLATION_MOUNTPOINT/home/$USERNAME/.inputrc

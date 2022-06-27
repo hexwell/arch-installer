ef17a82e2625394dec72b9b664ae251afe6f8939
Ettore Forigo <ettore.forigo@gmail.com>
2022-06-27 20:04:57 +0200

    Update xorg.bash

--- modules/personalize/xorg.bash
+++ modules/personalize/xorg.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+xorg_module() {
+
 # IMPORTS:
 #   out
 #   USER
@@ -8,15 +10,21 @@ out "[.] Installing Xorg."
 
 pacman --sync --needed --noconfirm xorg-server xorg-setxkbmap xautolock xss-lock xclipboard xorg-xrandr
 
-echo setxkbmap -layout it > /home/$USER/.xprofile
-echo >> /home/$USER/.xprofile
-echo /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 \& >> /home/$USER/.xprofile
-echo >> /home/$USER/.xprofile
-echo xautolock -time 10 -locker slock \& >> /home/$USER/.xprofile
-echo xss-lock slock \& >> /home/$USER/.xprofile
-echo >> /home/$USER/.xprofile
-echo xclipboard \& >> /home/$USER/.xprofile
+local xprofile=/home/$USER/.xprofile
+
+echo setxkbmap -layout it > $xprofile
+echo >> $xprofile
+echo /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 \& >> $xprofile
+echo >> $xprofile
+echo xautolock -time 10 -locker slock \& >> $xprofile
+echo xss-lock slock \& >> $xprofile
+echo >> $xprofile
+echo xclipboard \& >> $xprofile
 
 chown $USER /home/$USER/.xprofile
 
 out "[+] Xorg installed."
+
+}
+
+xorg_module

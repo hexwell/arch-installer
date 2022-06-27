b2c6a5ddc445e4b04386853bda34d70a6d8507b6
Ettore Forigo <ettore.forigo@gmail.com>
2022-06-27 19:19:10 +0200

    Update dm.bash

--- modules/personalize/dm.bash
+++ modules/personalize/dm.bash
@@ -10,7 +10,7 @@ sudo -u $USER yay -S --noconfirm ly
 
 systemctl enable ly
 
-sed --in-place 's/@tty2/@tty1/;s/tty2/tty7/' /usr/lib//systemd/system/ly.service
+sed --in-place 's/@tty2/@tty1/;s/tty2/tty7/' /usr/lib/systemd/system/ly.service
 sed --in-place '/#tty/s/^#//;s/tty = 2/tty = 7/' /etc/ly/config.ini
 
 out '[+] DM installed.'

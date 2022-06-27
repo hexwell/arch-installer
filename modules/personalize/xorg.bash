8c431529fc17548aa206a450bcfe41e1fd532683
Ettore Forigo <ettore.forigo@gmail.com>
2022-06-27 21:03:58 +0200

    Update xorg.bash

--- modules/personalize/xorg.bash
+++ modules/personalize/xorg.bash
@@ -11,13 +11,14 @@ out "[.] Installing Xorg."
 pacman --sync --needed --noconfirm xorg-server xorg-setxkbmap xautolock xss-lock xclipboard xorg-xrandr
 
 local xprofile=/home/$USER/.xprofile
+local locker=slock
 
 echo setxkbmap -layout it > $xprofile
 echo >> $xprofile
 echo /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 \& >> $xprofile
 echo >> $xprofile
-echo xautolock -time 10 -locker slock \& >> $xprofile
-echo xss-lock slock \& >> $xprofile
+echo xautolock -time 10 -locker $locker \& >> $xprofile
+echo xss-lock $locker \& >> $xprofile
 echo >> $xprofile
 echo xclipboard \& >> $xprofile
 

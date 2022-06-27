f74bb02c6dff4bff1e1bbfc8c9d0d3617396a241
Ettore Forigo <ettore.forigo@gmail.com>
2022-06-27 18:45:55 +0200

    Update utils.bash

--- modules/personalize/utils.bash
+++ modules/personalize/utils.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+utils_module() {
+
 # IMPORTS:
 #   out
 
@@ -7,9 +9,20 @@ out '[.] Installing utils.'
 
 pacman --sync --needed --noconfirm pulseaudio alsa-utils dmenu alacritty mate-polkit brightnessctl slock wget
 
-wget https://tools.suckless.org/dmenu/scripts/dmenu_run_with_command_history/dmenu_run_history --directory-prefix /usr/bin/
-chmod +x /usr/bin/dmenu_run_history
+local install_dir=/usr/local/bin/
+
+wget https://tools.suckless.org/dmenu/scripts/dmenu_run_with_command_history/dmenu_run_history --directory-prefix $install_dir
+wget https://raw.githubusercontent.com/LukeSmithxyz/voidrice/master/.local/bin/dmenumount --directory-prefix $install_dir
+wget https://raw.githubusercontent.com/LukeSmithxyz/voidrice/master/.local/bin/dmenuumount --directory-prefix $install_dir
+
+chmod +x $install_dir/dmenu_run_history
+chmod +x $install_dir/dmenumount
+chmod +x $install_dir/dmenuumount
 
 pacman --remove --recursive --noconfirm wget
 
 out '[+] Utils installed.'
+
+}
+
+utils_module

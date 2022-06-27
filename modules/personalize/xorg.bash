5ad802eb05a9b61106deac1262436bba5709bf4a
Ettore Forigo <ettore.forigo@gmail.com>
2022-06-27 14:39:11 +0200

    Update xorg.bash

--- modules/personalize/xorg.bash
+++ modules/personalize/xorg.bash
@@ -11,6 +11,8 @@ pacman --sync --needed --noconfirm xorg-server
 echo setxkbmap -layout it > /home/$USER/.xprofile
 echo >> /home/$USER/.xprofile
 echo /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 \& >> /home/$USER/.xprofile
+echo >> /home/$USER/.xprofile
+echo udiskie \& >> /home/$USER/.xprofile
 
 chown $USER /home/$USER/.xprofile
 

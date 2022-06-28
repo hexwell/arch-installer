af498bb8a8b3b449abfb9e7455d93eb5bd38d42d
Ettore Forigo <ettore.forigo@gmail.com>
2022-06-28 06:54:31 +0200

    Update utils.bash

--- modules/personalize/utils.bash
+++ modules/personalize/utils.bash
@@ -11,14 +11,8 @@ pacman --sync --needed --noconfirm pulseaudio alsa-utils dmenu alacritty mate-po
 
 local install_dir=/usr/local/bin/
 
-download() {
-	wget $1 --directory-prefix $install_dir
-}
-
-download https://tools.suckless.org/dmenu/scripts/dmenu_run_with_command_history/dmenu_run_history
-download https://raw.githubusercontent.com/LukeSmithxyz/voidrice/master/.local/bin/dmenumount
-download https://raw.githubusercontent.com/LukeSmithxyz/voidrice/master/.local/bin/dmenuumount
-
+wget https://tools.suckless.org/dmenu/scripts/dmenu_run_with_command_history/dmenu_run_history https://raw.githubusercontent.com/LukeSmithxyz/voidrice/master/.local/bin/dmenumount https://raw.githubusercontent.com/LukeSmithxyz/voidrice/master/.local/bin/dmenuumount --directory-prefix $install_dir
+ 
 chmod +x $install_dir/*
 
 pacman --remove --recursive --noconfirm wget

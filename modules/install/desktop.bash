4b6ccf239c77896fcaa188acbb334de589d1e0b3
Ettore Forigo <ettore.forigo@gmail.com>
2022-06-28 07:12:10 +0200

    Update desktop.bash

--- modules/install/desktop.bash
+++ modules/install/desktop.bash
@@ -15,7 +15,7 @@ local homedir=/home/$USERNAME/
 
 cp --recursive $REPOSITORY_LOCATION$REPOSITORY_NAME $INSTALLATION_MOUNTPOINT$homedir
 
-$chroot bash -c "cd $homedir$REPOSITORY_NAME; ./personalize.bash" user_stdout >&$user_stdout
+$chroot bash -c "cd $homedir$REPOSITORY_NAME; ./personalize.bash" user_stdout &>&$user_stdout
 
 }
 

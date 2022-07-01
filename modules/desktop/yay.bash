99b316b5508e129adb25a7d285ce2aa3c543c217
Ettore Forigo <ettore.forigo@gmail.com>
2022-07-01 16:53:57 +0200

    reintroduce installation of dependencies, use fork of docker-archlinux-aur for add-aur.sh

--- modules/desktop/yay.bash
+++ modules/desktop/yay.bash
@@ -6,8 +6,8 @@
 
 out '[.] Installing yay...'
 
-# $install --refresh base-devel
+$install --refresh base-devel
 
-curl https://raw.githubusercontent.com/greyltc-org/docker-archlinux-aur/master/add-aur.sh | bash
+curl https://raw.githubusercontent.com/hexwell/docker-archlinux-aur/master/add-aur.sh | bash
 
 out '[+] yay installed.'

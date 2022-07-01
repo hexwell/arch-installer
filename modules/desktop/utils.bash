6420c5709df151ae5d6dbc5da7d7b2374749b99d
Ettore Forigo <ettore.forigo@gmail.com>
2022-07-01 12:35:57 +0200

    add udisks2 & explanation of the use of dmenumount

--- modules/desktop/utils.bash
+++ modules/desktop/utils.bash
@@ -10,9 +10,12 @@ utils_module() {
 
 out '[.] Installing utils...'
 
-$install pulseaudio alsa-utils dmenu alacritty brightnessctl $LOCKER_PKG mate-polkit wget
+$install pulseaudio alsa-utils dmenu alacritty brightnessctl $LOCKER_PKG udisks2 mate-polkit wget
 
 local install_dir=/usr/local/bin/
+
+# These mounting scripts are used instead of udiskie (for example) to allow for manual control of mounting,
+# and because udiskie only auto mounts disks, so unmount must be done manually and a helper would have been necessary anyways.
 
 wget https://tools.suckless.org/dmenu/scripts/dmenu_run_with_command_history/dmenu_run_history https://raw.githubusercontent.com/hexwell/voidrice/master/.local/bin/dmenumount https://raw.githubusercontent.com/hexwell/voidrice/master/.local/bin/dmenuumount --directory-prefix $install_dir
 

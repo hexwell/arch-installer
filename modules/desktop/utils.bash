26faeb132db5387460e793a33308e0e757668b1f
Ettore Forigo <ettore.forigo@gmail.com>
2022-07-01 13:29:03 +0200

    remove unnecessary wget

--- modules/desktop/utils.bash
+++ modules/desktop/utils.bash
@@ -6,22 +6,19 @@ utils_module() {
 #   out
 #   install
 #   LOCKER_PKG
-#   silent_pacman
 
 out '[.] Installing utils...'
 
-$install pulseaudio alsa-utils dmenu alacritty brightnessctl $LOCKER_PKG udisks2 mate-polkit wget
+$install pulseaudio alsa-utils dmenu alacritty brightnessctl $LOCKER_PKG udisks2 mate-polkit
 
 local install_dir=/usr/local/bin/
 
 # These mounting scripts are used instead of udiskie (for example) to allow for manual control of mounting,
 # and because udiskie only auto mounts disks, so unmount must be done manually and a helper would have been necessary anyways.
 
-wget https://tools.suckless.org/dmenu/scripts/dmenu_run_with_command_history/dmenu_run_history https://raw.githubusercontent.com/hexwell/voidrice/master/.local/bin/dmenumount https://raw.githubusercontent.com/hexwell/voidrice/master/.local/bin/dmenuumount --directory-prefix $install_dir
+curl --output-dir $install_dir --remote-name-all https://tools.suckless.org/dmenu/scripts/dmenu_run_with_command_history/dmenu_run_history https://raw.githubusercontent.com/hexwell/voidrice/master/.local/bin/dmenumount https://raw.githubusercontent.com/hexwell/voidrice/master/.local/bin/dmenuumount
 
 chmod +x $install_dir/*
-
-$silent_pacman --remove --recursive wget
 
 out '[+] Utils installed.'
 
